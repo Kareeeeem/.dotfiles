adbf85ae90a6ad877caf629136224e1681f25da4
kareeeeem <kareeeeem@gmail.com>
2015-03-23 12:01:36 +0100

    simpler prompt test

--- git-prompt.zsh
+++ git-prompt.zsh
@@ -12,20 +12,23 @@ parse_git_branch() {
 parse_git_state() {
 
     # Compose this value via multiple conditional appends.
-    local GIT_STATE=""
-
-
-    if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
-        GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
+    local GIT_STATE="%F{green}"
+    if [[ -n $(git status --porcelain 2> /dev/null) ]]; then
+        GIT_STATE="%F{red}"
     fi
 
-    if ! git diff --quiet 2> /dev/null; then
-        GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
-    fi
 
-    if ! git diff --cached --quiet 2> /dev/null; then
-        GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
-    fi
+    # if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+    #     GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
+    # fi
+
+    # if ! git diff --quiet 2> /dev/null; then
+    #     GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
+    # fi
+
+    # if ! git diff --cached --quiet 2> /dev/null; then
+    #     GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
+    # fi
 
     if [[ -n $GIT_STATE ]]; then
         echo "$GIT_STATE"
@@ -33,7 +36,12 @@ parse_git_state() {
 }
 
 # If inside a Git repository, print its branch and state
+# git_prompt_string() {
+#     local git_where="$(parse_git_branch)"
+#     [ -n "$git_where" ] && echo "git:${git_where#(refs/heads/|tags/)}$(parse_git_state) "
+# }
+
 git_prompt_string() {
     local git_where="$(parse_git_branch)"
-    [ -n "$git_where" ] && echo "git:${git_where#(refs/heads/|tags/)}$(parse_git_state) "
+    [ -n "$git_where" ] && echo "git:$(parse_git_state)${git_where#(refs/heads/|tags/)}%f "
 }
