25420ac1afa8ed4e18bfa06947937301f5ebbfe6
Mohammed Kareem <kareeeeem@gmail.com>
2016-04-09 19:30:39 +0200

    script for opening ports

--- setup.sh
+++ setup.sh
@@ -1,14 +1,27 @@
 #! /usr/bin/env sh
 
-# Setup a debian chroot on my chromebook with (almosg) everything I want.
-# might have run twice for all the installs to work
+# Setup a debian chroot on my chromebook with (almost) everything I want.
+# might have run twice for all the installs to work.
 
 set -e
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y \
-    vim-nox build-essential cmake python-dev python3-dev postgresql \
-    libffi-dev libpq-dev git man curl zsh openssh-server tmux wget \
-	exuberant-ctags silversearcher-ag
+    vim-nox \
+	cmake \
+	python-dev \
+	python3-dev \
+	postgresql \
+    libffi-dev \
+	git \
+	man \
+	curl \
+	zsh \
+	openssh-server \
+	tmux \
+	wget \
+	exuberant-ctags \
+	silversearcher-ag \
+	par
 
 echo "enter your email (for ssh key generation and git), followed by [ENTER]."
 read EMAIL
@@ -16,7 +29,9 @@ read EMAIL
 ssh-keygen -t rsa -b 4096 -C "$EMAIL"
 
 cat "$HOME/.ssh/id_rsa.pub"
-read -p "added key to github account?" reply
+
+# Prompt user to add key to github account. So we can clone with ssh.
+read -p "Add key to github account and then press enter." reply
 
 git config --global user.email "$EMAIL"
 git config --global user.name "Mohammed Kareem"
