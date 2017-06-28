77fb4d6390f5bd0efe8404bc6c5b8f4a8764d6e3
Mohammed Kareem <kareeeeem@gmail.com>
2017-06-28 11:47:11 +0200

    proper entrypoint

--- autoenv.sh
+++ autoenv.sh
@@ -1,37 +1,36 @@
 #!/usr/bin/env bash
 
 # Auto activates environments by sourcing .env files. Env files are
-# authenticated by checking a checksum.  If envfile provides a function
-# _deactivate calling _deactivate_env or the alias deac will call it.
+# authenticated by validating them against a checksum.  If envfile provides a
+# function _deactivate calling deactivating the environment will call it.
 
+# The filename of environment files
 AUTOENV_ENVFILE=.env
+# Where it will store checksums
 AUTOENV_CHECKSUM=~/.envsum
+
+# Used as a heuristic to see if we want to check for environment files.
 AUTOENV_LAST_PWD=
+
 export AUTOENV_PROMPT=
 export AUTOENV=
 
-if [ ! -d "$AUTOENV_CHECKSUM" ]; then
-    mkdir -p "$AUTOENV_CHECKSUM"
-fi
+# create the checksum directory if it does not exist
+[ ! -d "$AUTOENV_CHECKSUM" ] && mkdir -p "$AUTOENV_CHECKSUM"
 
-# Helpers
-# -----------------------------------------------------------------------------
+# HELPERS
 
-_file_exists () {
-	stat -t "$1" > /dev/null 2>&1
-}
-
-_checksum_file () {
-    echo -n "$PWD" | md5sum | awk '{print $1}'
+_checksum_filename () {
+    echo "$AUTOENV_CHECKSUM/$(echo -n "$PWD" | md5sum | awk '{print $1}')"
 }
 
 _envfile_is_authorized () {
-    _file_exists "$AUTOENV_CHECKSUM/$(_checksum_file)" && \
-        md5sum -c "$AUTOENV_CHECKSUM/$(_checksum_file)" > /dev/null 2>&1
+    checksum=$(_checksum_filename)
+    [ -f "$checksum" ] && md5sum -c "$checksum" < $AUTOENV_ENVFILE > /dev/null 2>&1
 }
 
 _authorize_envfile () {
-    md5sum "$AUTOENV_ENVFILE" > "$AUTOENV_CHECKSUM/$(_checksum_file)"
+    md5sum < $AUTOENV_ENVFILE > "$(_checksum_filename)"
 }
 
 _confirm_envfile () {
@@ -49,38 +48,23 @@ _confirm_envfile () {
 }
 
 
-# "main" function
-# -----------------------------------------------------------------------------
-
+# MAIN
 _autoenv () {
 	[ "$AUTOENV_LAST_PWD" = "$PWD" ] && return
 	AUTOENV_LAST_PWD=$PWD
 
 	[ -n "$AUTOENV" ] && return
 
-	_activate
+	_activate_environment
 }
 
-# (de)activation
-# -----------------------------------------------------------------------------
-
-_activate_environment () {
-	if _envfile_is_authorized || _confirm_envfile; then
-		if _authorize_envfile; then
-            AUTOENV="$PWD/$AUTOENV_ENVFILE"
-            . "$AUTOENV_ENVFILE"
-        fi
-	fi
-}
+# (DE/RE)ACTIVATION
 
 _deactivate_environment () {
 	AUTOENV=
 	AUTOENV_PROMPT=
     _deactivate;
     unset -f _deactivate
-	# If the envfile defined _deactivate call it.
-    # if [ -n "$(declare -f -F _deactivate)" ]; then
-    # fi
 }
 
 _reactivate_environment () {
@@ -89,19 +73,24 @@ _reactivate_environment () {
 	AUTOENV_LAST_PWD=
 }
 
-# active from child directory
-_activate () {
+_activate_environment () {
 	local slashes current_dir original_dir
 
 	original_dir="$PWD"
 	current_dir="$original_dir"
 	slashes=${current_dir//[^\/]/}
 
+    # Bubble up the PWD to find an .env file
 	for (( n=${#slashes}; n>1; --n )); do
-		if _file_exists "$current_dir/$AUTOENV_ENVFILE"; then
+		if [ -f "$current_dir/$AUTOENV_ENVFILE" ]; then
 			cd "$current_dir"
 
-			_activate_environment
+            if _envfile_is_authorized || _confirm_envfile; then
+                if _authorize_envfile; then
+                    AUTOENV="$PWD/$AUTOENV_ENVFILE"
+                    . "$AUTOENV_ENVFILE"
+                fi
+            fi
 
 			cd "$original_dir"
 			return
@@ -111,8 +100,11 @@ _activate () {
 	done
 }
 
-showenv () {
-	if [ -n "$AUTOENV" ] && _file_exists "$AUTOENV"; then
+# utils
+# -----------------------------------------------------------------------------
+
+_showenv () {
+	if [ -n "$AUTOENV" -a -f "$AUTOENV" ]; then
 		cat "$AUTOENV"
 		echo "$AUTOENV"
 	else
@@ -120,15 +112,15 @@ showenv () {
 	fi
 }
 
-editenv () {
-	if [ -n "$AUTOENV" ] && _file_exists "$AUTOENV"; then
+_editenv () {
+	if [ -n "$AUTOENV" -a -f "$AUTOENV" ]; then
 		$EDITOR "$AUTOENV"
 	else
 		echo "No environment sourced or envfile has been moved or deleted."
 	fi
 }
 
-# Set up the prompt command and aliases
+# Set up the prompt command and entrypoint
 # -----------------------------------------------------------------------------
 
 if [ -z "$_AUTOENV_NO_PROMPT_COMMAND" ]; then
@@ -139,5 +131,16 @@ fi
 if [ -z "$_AUTOENV_NO_ALIASES" ]; then
 	alias deac="_deactivate_environment"
 	alias reac="_reactivate_environment"
-	alias ac="_activate"
+	alias ac="_activate_environment"
 fi
+
+e () {
+    case $1 in
+        "ac") _activate_environment ;;
+        "de") _deactivate_environment ;;
+        "re") _deactivate_environment && _activate_environment ;;
+        "show") _showenv ;;
+        "edit") _editenv ;;
+        *) echo "[USAGE] e ac|de|re" ;;
+    esac
+}
